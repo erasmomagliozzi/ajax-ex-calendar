20e36ac68f06af90078a47b9d0a8bfc60943982a
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-02-03 17:15:45 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 $(document).ready(function(){
-  // $.ajax({
-  //  url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
-  //  method: "GET",
-  //  success: function (data, stato) {
-  //    console.log(data);
-  //
-  //  },
-  // error: function (richiesta, stato, errori) {
-  //     alert("E' avvenuto un errore. " + errore);
-  //  }
-  // });
+  $.ajax({
+   url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+   method: "GET",
+   success: function (data, stato) {
+     var festivita = data.response;
+     console.log(festivita);
+
+   },
+  error: function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errore);
+   }
+  });
 
 
   var daysInMonth = moment("2018-01", "YYYY-MM").daysInMonth();
   console.log(daysInMonth);
 
   for (var i = 1; i <= daysInMonth; i++){
+
     var source = $("#entry-template").html();
     var template = Handlebars.compile(source);
     var context = { day: i };
